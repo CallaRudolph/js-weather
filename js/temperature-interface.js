1c07965f5fd3c0939c8e02d41f6ef033891bacd6
Calla Rudolph and Nathan Stewart <calla@>
2017-08-03 08:35:00 -0700

    refactor temp btns and api calls into a single btn and api call

--- js/temperature-interface.js
+++ js/temperature-interface.js
@@ -1,34 +1,14 @@
 var Temperature = require('./../js/temperature.js').temperatureModule;
 
-var displayTemperature = function(city, tempData) {
-  $('.showTemperature').text("The temperature in " + city + " is " + tempData + " degrees Kelvin");
-};
-
-var displayTemperatureCelsius = function(city, tempData) {
-  $('.showTemperatureC').text("The temperature in " + city + " is " + tempData + " degrees Celsius");
-};
-
-var displayTemperatureFahrenheit = function(city, tempData) {
-  $('.showTemperatureF').text("The temperature in " + city + " is " + tempData + " degrees Fahrenheit");
+var displayTemperature = function(city, tempData, tempDataC, tempDataF) {
+  $('.showTemperature').text("The temperature in " + city + " is " + tempData + " degrees Kelvin, " + tempDataC + " degrees Celsius, and " + tempDataF + " degrees Fahrenheit.");
 };
 
 $(document).ready(function() {
   var currentTemperatureObject = new Temperature();
-  $('#weatherTK-location').click(function() {
+  $('#weatherT-location').click(function() {
     var city = $('#location').val();
     // $('#location').val("");
     currentTemperatureObject.getTemperature(city, displayTemperature);
-  });
-
-  $('#weatherTC-location').click(function() {
-    var city = $('#location').val();
-    // $('#location').val("");
-    currentTemperatureObject.getTemperatureC(city, displayTemperatureCelsius);
-  });
-
-  $('#weatherTF-location').click(function() {
-    var city = $('#location').val();
-    // $('#location').val("");
-    currentTemperatureObject.getTemperatureF(city, displayTemperatureFahrenheit);
   });
 });
--- js/temperature.js
+++ js/temperature.js
@@ -1,31 +1,13 @@
 var apiKey = require('./../.env').apiKey;
-Temperature = function() {
 
+Temperature = function() {
 };
 
 Temperature.prototype.getTemperature = function(city, displayTemperature) {
   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-    displayTemperature(city, response.main.temp);
+    displayTemperature(city, response.main.temp, Math.round(response.main.temp - 273), Math.round(1.8 * (response.main.temp - 273) + 32));
   }).fail(function(error) {
     $('.showTemperature').text(error.responseJSON.message);
-  });
-};
-
-Temperature.prototype.getTemperatureC = function(city, displayTemperatureCelsius) {
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-    var celsius = Math.round(response.main.temp - 273);
-    displayTemperatureCelsius(city, celsius);
-  }).fail(function(error) {
-    $('.showTemperatureC').text(error.responseJSON.message);
-  });
-};
-
-Temperature.prototype.getTemperatureF = function(city, displayTemperatureFahrenheit) {
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-    var fahrenheit = Math.round(1.8 * (response.main.temp - 273) + 32);
-    displayTemperatureFahrenheit(city, fahrenheit);
-  }).fail(function(error) {
-    $('.showTemperatureF').text(error.responseJSON.message);
   });
 };
 
